a211eb50c02ff24b96dd48dd52819a22acb5a377
[abigaile] <abigailelewis57@gmail.com>
2018-12-14 14:31:22 -0500

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,9 @@ import { NgModule } from '@angular/core';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TravallComponent} from './travall/travall.component';
 import { AuthGuard } from './help/auth.guard';
+import { CreateComponent } from './create/create.component';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/about', pathMatch: 'full' },
@@ -22,6 +24,15 @@ const appRoutes: Routes = [
     { path: 'about', component: AboutComponent, data: {title: 'About'}},
     { path: 'login', component: LoginComponent,  data: {title: 'Login'} },
     { path: 'signup', component: SignupComponent , data: {title: 'Siginup'} },
+    { path: 'travall', component: TravallComponent , data: {title: 'Travall'},
+        children: [{
+        
+            path:'create',
+            component: CreateComponent
+        },
+        ]
+
+},
 
     // otherwise redirect to about
     // { path: '**', redirectTo: '' }
