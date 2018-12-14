ca6d07e86e64549473fee4360a7bf1fbf72c11ed
Jayshon <realj2134@gamil.com>
2018-12-14 12:02:44 -0500

    create changes

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,37 +1,36 @@
-// import { Component, OnInit, NgZone } from '@angular/core';
-// import { MapsService } from '../services/maps.service';
+ import { Component, OnInit, NgZone } from '@angular/core';
+ import { MapsService } from '../services/maps.service';
 
-// @Component({
-//   selector: 'app-create',
-//   templateUrl: './create.component.html',
-//   styleUrls: ['./create.component.css']
-// })
-// export class CreateComponent implements OnInit {
-//   title: string = 'My first AGM project';
-//   lat: string = '';
-//   lng: string = '';
+ @Component({
+   selector: 'app-create',
+   templateUrl: './create.component.html',
+   styleUrls: ['./create.component.css']
+ })
+ export class CreateComponent implements OnInit {
+   title: string = 'My first AGM project';
+   lat: string = '';
+   lng: string = '';
 
-//   public addrKeys: string[];
-//   public addr: object;
-//   public addrObj: any;
+   public addrKeys: string[];
+   public addr: object;
 
-//   location: Object;
+   location: Object;
 
-//   setAddress(addrObj) {
-//     this.zone.run(() => {
-//       this.addr = addrObj;
-//       this.addrKeys = Object.keys(addrObj);
-//     });
-//   }
+   setAddress(addrObj) {
+     this.zone.run(() => {
+       this.addr = addrObj;
+       this.addrKeys = Object.keys(addrObj);
+     });
+   }
 
-//   constructor(private map: MapsService, private zone: NgZone) { }
+   constructor(private map: MapsService, private zone: NgZone) { }
 
-//   ngOnInit() {
-//     this.map.getLocation().subscribe(data =>{
-//       console.log(data);
-//       this.lat = data.latitude;
-//       this.lng = data.longitude;
-//     })
-//   }
+   ngOnInit() {
+     this.map.getLocation().subscribe(data =>{
+       console.log(data);
+       this.lat = data.latitude;
+       this.lng = data.longitude;
+     })
+   }
 
-// }
+ }
