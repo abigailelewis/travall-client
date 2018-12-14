70c08f6d736364ac72f11543f61aecb7f50c9672
[abigaile] <abigailelewis57@gmail.com>
2018-12-14 17:17:05 -0500

    cleanup

--- webpack.config.ts
+++ webpack.config.ts
@@ -34,16 +34,3 @@ module.exports = {
         historyApiFallback: true
     }
 };
-// import path from 'path';
-// import webpack from 'webpack';
-
-// const config: webpack.Configuration = {
-//   mode: 'production',
-//   entry: './foo.js',
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: 'foo.bundle.js'
-//   }
-// };
-
-// export default config;
