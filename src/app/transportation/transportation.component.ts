49e2834605378486392aae9b54509d512f0b0377
[abigaile] <abigailelewis57@gmail.com>
2018-12-18 02:01:13 -0500

    crew cleanup/ transportation table/pull

--- src/app/transportation/transportation.component.ts
+++ src/app/transportation/transportation.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TransportationService } from '@/services/transportation.service';
 import { CreateTransportComponent } from '../create-transport/create-transport.component';
 import { MatDialog } from '@angular/material';
-
+import { Transport } from '../models/transport';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-transportation',
   templateUrl: './transportation.component.html',
@@ -12,7 +13,7 @@ export class TransportationComponent implements OnInit {
   currentTravall: any = JSON.parse(sessionStorage.getItem('currentTravall')) || '';
   transports: any = [];
 
-  constructor(private transportService: TransportationService, private dialog: MatDialog) { }
+  constructor(private transportService: TransportationService, private dialog: MatDialog, private http: HttpClient) { }
 
   ngOnInit() {
     this.getTransports();
