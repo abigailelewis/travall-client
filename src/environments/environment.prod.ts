354fcd2f6f5589be6b1adabe90e480aa9db07ec4
Connor Mellencamp <connorsmac@Connors-MacBook-Pro.local>
2018-12-18 18:35:10 -0500

    push to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export let APIURL = '';
+​
+switch (window.location.hostname) {
+    case 'https://travall-client.herokuapp.com/':
+        APIURL = 'https://travall-server.herokuapp.com'
+        break;
+    default:
+        APIURL = 'http://localhost:4200';
+}
